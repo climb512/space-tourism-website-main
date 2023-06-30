7a0e0f01c3ff14ed7ef358607cc7714b5f6fbb76
Ted <climb512@gmail.com>
2023-06-30 09:39:19 +0700

    destination-moon

--- scripts/script.js
+++ scripts/script.js
@@ -6,9 +6,10 @@ navToggle.addEventListener("click", () => {
     const visiblity = nav.getAttribute("data-visible");
     if (visiblity === "false") {
         nav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
     } else {
         nav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
     }
-    
-    console.log(visiblity);
+
 })
